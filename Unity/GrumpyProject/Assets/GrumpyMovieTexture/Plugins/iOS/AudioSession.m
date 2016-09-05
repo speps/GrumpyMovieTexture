82d037d7de77c370cca1142d55705b2bb3a50cdb
Remi Gillig <remigillig@gmail.com>
2016-09-05 22:00:59 +0100

    Update submitted binaries.

--- Unity/GrumpyProject/Assets/GrumpyMovieTexture/Plugins/iOS/AudioSession.m
+++
@@ -1,8 +0,0 @@
-#import <AVFoundation/AVAudioSession.h>
-
-void AudioSessionSetup()
-{
-    NSError *error = nil;
-    AVAudioSession *session = [AVAudioSession sharedInstance];
-    [session setCategory:AVAudioSessionCategoryPlayback error:&error];
-}
